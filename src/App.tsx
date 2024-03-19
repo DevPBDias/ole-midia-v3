51003768264cd067817dd0ea3f01909ff0ab1558
DevPBDias <devpbdias@gmail.com>
2024-03-19 09:12:17 -0300

    feat: created who component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header'
 import Home from './components/home'
+import Who from './components/who'
 
 function App() {
   return (
       <>
         <Header />
         <Home />
+        <Who />
       </>
   )
 }
---
+++ src/components/who/styles.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const WhoContainer = styled.section`
+    width: 100%;
+    height: 32em;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+`;
+
+export const ImgContainer = styled.div`
+    width: 100%;
+    height: 14.67em;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export const TxtContainer = styled.div`
+    width: 100%;
+    height: calc(32em - 14.67em);
+    padding-block: 2.7em;
+
+    div {
+        width: 72%;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+        text-align: center;
+        gap: 1em;
+
+        h1 {
+            color: var(--Vibrant-Orange, #FF6123);
+            font-size: 1.77778rem;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 2rem; 
+        }
+
+        p {
+            color: var(--Dark-Brown, #250900);
+            text-align: center;
+            font-size: 0.66667rem;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 1.33333rem;
+
+            span {
+                color: var(--Dark-Brown, #250900);
+                font-size: 0.66667rem;
+                font-style: normal;
+                font-weight: 700;
+                line-height: 1.33333rem;
+            }
+        }
+    }
+`;
