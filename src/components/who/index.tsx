e6b3dd944815eafb739c40206c1cd0cb588f63b9
DevPBDias <devpbdias@gmail.com>
2024-03-24 23:34:18 -0300

    styles:  responsive who

--- src/components/who/index.tsx
+++ src/components/who/index.tsx
@@ -1,22 +1,22 @@
-import olemidia from '../../assets/images/who_mobile.webp'
+import olemidiaMobile from '../../assets/images/who_mobile.webp'
 import { ImgContainer, TxtContainer, WhoContainer } from './styles'
 
 function Who() {
-  return (
-    <WhoContainer>
-        <ImgContainer>
-            <img src={olemidia} alt="" />
-        </ImgContainer>
-        <TxtContainer>
-            <div>
-                <h1>Quem somos</h1>
-                <p>
-                    A <span>Olé Midia</span> surgiu da união de duas paixões entre primos: esporte e design. Nossa missão é elevar ao máximo a imagem de nossos atletas. Oferecemos serviços de assessoria, coordenação, gestão e planejamento como parte integrante de nossa atuação profissional.
-                </p>
-            </div>
-        </TxtContainer>
-    </WhoContainer>
-  )
+    return (
+        <WhoContainer>
+            <ImgContainer>
+                <img src={olemidiaMobile} alt="oleMidia" />
+            </ImgContainer>
+            <TxtContainer>
+                <div>
+                    <h1>Quem somos</h1>
+                    <p>
+                        A <span>Olé Midia</span> surgiu da união de duas paixões entre primos: esporte e design. Nossa missão é elevar ao máximo a imagem de nossos atletas. Oferecemos serviços de assessoria, coordenação, gestão e planejamento como parte integrante de nossa atuação profissional.
+                    </p>
+                </div>
+            </TxtContainer>
+        </WhoContainer>
+    )
 }
 
 export default Who
--- src/components/who/styles.tsx
+++ src/components/who/styles.tsx
@@ -7,20 +7,45 @@ export const WhoContainer = styled.section`
     justify-content: center;
     align-items: center;
     flex-shrink: 0;
+
+    
+    @media (min-width: 600px) {
+        flex-flow: row nowrap;
+        height: 26.2rem;
+    }
+    
+    @media (min-width: 760px) {
+        height: 22.2rem;
+    }
+
+    @media (min-width: 1000px) {
+        height: 35.5rem;
+    }
 `;
 
-export const ImgContainer = styled.div`
+export const ImgContainer = styled.picture`
     width: 100%;
+    height: 100%;
 
     img {
         width: 100%;
         height: 100%;
     }
+
+    @media (min-width: 600px) {
+        width: 50%;
+    }
+
+    @media (min-width: 750px) {
+        width: 55%;
+    }
 `;
 
 export const TxtContainer = styled.div`
     width: 100%;
-    padding-block: 3em;
+    display: flex;
+    justify-items: center;
+    align-items: center;
 
     div {
         padding-inline: 10%;
@@ -56,5 +81,78 @@ export const TxtContainer = styled.div`
                 line-height: 1.33333rem;
             }
         }
+    }
+
+    @media (min-width: 600px) {
+        width: 50%;
+        padding-inline: 3em;
+        padding-block: 2.7em;
+
+        div {
+            width: 100%;
+            padding-inline: 0;
+            align-items: flex-start;
+
+        h1 {
+            width: 4.22rem;
+            font-size: 2.66667rem;
+            line-height: 3.11111rem;
+            text-align: left;
+        }
+
+        p {
+            width: 12.6rem;
+            text-align: left;
+        }
+    }
+
+    }
+
+    @media (min-width: 750px) {
+        width: 45%;
+
+        div {
+            p {
+                width: 14.88rem;
+            }
+        }
+
+    }
+
+    @media (min-width: 1000px) {
+        width: 45%;
+
+        div {
+            padding-inline: 10%;
+            align-items: flex-start;
+
+        h1 {
+            width: 6.67rem;
+            font-size: 3.55556rem;
+            line-height: 4rem; 
+        }
+
+        p {
+            width: 17.88rem;
+            text-align: left;
+            font-size: 1rem;
+            line-height: 1.77778rem;
+
+            span {
+                font-size: 1rem;
+                line-height: 1.77778rem;
+            }
+        }
+    }
+
+    }
+
+    @media (min-width: 1250px) {
+        div {
+            p {
+                width: 20.88rem;
+            }
+        }
+
     }
 `;
