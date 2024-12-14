168fced1fff2e0a58a26b4f9908f355bb41c74ac
DevPBDias <devpbdias@gmail.com>
2024-12-14 19:02:48 -0300

    feat: removed old clients and add new client change luis photo

--- src/data/clientsData.ts
+++ src/data/clientsData.ts
@@ -2,17 +2,13 @@ import image1 from "../assets/images/giu.webp";
 import image2 from "../assets/images/leo.webp";
 import image3 from "../assets/images/roni.webp";
 import image4 from "../assets/images/sandrinho.webp";
-import image5 from "../assets/images/raquel.webp";
-import image6 from "../assets/images/heman.webp";
 import image7 from "../assets/images/Luis_Eduardo.png";
 import image1_dk from "../assets/images/giu_dk.webp";
 import image2_dk from "../assets/images/leo.webp";
 import image3_dk from "../assets/images/roni_dk.webp";
 import image4_dk from "../assets/images/sandrinho_dk.webp";
-import image5_dk from "../assets/images/raquel_dk.webp";
-import image6_dk from "../assets/images/heman_dk.webp";
 import image7_dk from "../assets/images/Luis_Eduardo_dk.png";
-
+import image8_dk from "../assets/images/Gabardo_dk.png";
 
 export type IClientProps = {
   id: number;
@@ -53,24 +49,17 @@ const clientsData: IClientProps[] = [
   },
   {
     id: 5,
-    name: "Raquel Fernandes",
-    team: "Grêmio",
-    image: image5,
-    image_dk: image5_dk,
-  },
-  {
-    id: 6,
-    name: "Rafael Moura",
-    team: "Ex-Atleta",
-    image: image6,
-    image_dk: image6_dk,
-  },
-  {
-    id: 7,
     name: "Luis Eduardo",
     team: "Goiás (U-17)",
     image: image7,
     image_dk: image7_dk,
+  },
+  {
+    id: 6,
+    name: "Gabardo Jr.",
+    team: "ABECAT (Treinardor)",
+    image: image7,
+    image_dk: image8_dk,
   },
 ];
 
